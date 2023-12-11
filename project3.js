10edf17a356a6aa45ef9e9a3a17fe760db4ae1a6
yunuSdet <yunuskulcu84@gmail.com>
2023-12-11 15:36:01 +0300

    project3 is added

---
+++ project3.js
@@ -0,0 +1,25 @@
+/*
+[7, 5, 1, 8, 3, 6, 0, 9, 4, 2]  -> Binary Search Tree Aşamaları:
+
+
+Aşama 1: Root elemanı 7 olarak belirlenir.
+
+Aşama 2: 5, 7'den küçük olduğu için sola yazılır.
+
+Aşama 3: 1, 5'ten küçük olduğu için sola yazılır.
+
+Aşama 4: 8, 7'den büyük olduğu için sağa yazılır.
+
+Aşama 5: 3, 7'den küçük olduğu için sola yazılır.
+
+Aşama 6: 6, 5'ten büyük olduğu için sağa yazılır.
+
+Aşama 7: 0, 7'den küçük olduğu için sola yazılır.
+
+Aşama 8: 9, 7'den büyük olduğu için sağa yazılır.
+
+Aşama 9: 4, 7'den büyük olduğu için sağa yazılır.
+
+Aşama 10: 2, 7'den küçük olduğu için sola yazılır.
+
+*/
